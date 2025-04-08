87c452e8f2d5c179e3f6124afe30e1fd9621e5ac
Miguel <49378405+SchiieBen@users.noreply.github.com>
2025-04-08 02:07:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string? Digitei = Console.ReadLine();
+string[] partes = Digitei.Split(' ');
+
+int a = int.Parse(partes[0]);
+string operador = partes[1];
+int b = int.Parse(partes[2]);
+Console.WriteLine(Calculos(a, b, operador));
+
+static int Calculos(int a, int b, string operador)
+{
+    if (operador == "+")
+        return a + b;
+    else if (operador == "-")
+        return a - b;
+    else if (operador == "*")
+        return a * b;
+    else if (operador == "/")
+        return a / b;
+    else
+        throw new Exception("Operador inválido");
+}
